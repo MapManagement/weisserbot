451b6e081c393fc16abb86c4ac07fb2aff22da50
MapManagement <fortnitetest@mail.de>
2020-08-06 03:14:05 +0200

    removed raise_for_status

--- bot/oauth.py
+++ bot/oauth.py
@@ -5,7 +5,6 @@ from bot.utils import secrets
 def get_oauth(scope: str) -> str:
     try:
         response = requests.post(secrets.tokenurl + scope)
-        response.raise_for_status()
         json_response = response.json()
         return json_response["access_token"]
 
